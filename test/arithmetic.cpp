b1753824f6cc90bd43aa2f2178fc162a45e5291e
John Bytheway <jbytheway@gmail.com>
2009-02-08 12:55:28 +0000

    Made test/arithmetic.cpp conform to same style as other tests
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- test/arithmetic.cpp
+++ test/arithmetic.cpp
@@ -55,29 +55,29 @@ BOOST_AUTO_TEST_CASE(arithmetic_grammar)
       sh::production<expression, X>,
       sh::production<expression, expression, sh::discard<PLUS>, X>
     >::type
-  >::type grammar_type;
+  >::type Grammar;
   // Construct the grammar
-  grammar_type arithmetic_grammar;
+  Grammar grammar;
   try {
-    arithmetic_grammar.check();
+    grammar.check();
   } catch (sh::grammar_exception& e) {
     BOOST_ERROR(std::string("grammar exception: ") + e.what());
   }
   std::cout << "productions:\n";
-  arithmetic_grammar.dump_productions(std::cout);
+  grammar.dump_productions(std::cout);
   // The parser type is similarly horrible, but obtainable from the grammar
   // type so we don't need to express all that stuff again.
-  typedef sh::lalr::parser_from_grammar<grammar_type> parser_type;
-  parser_type arithmetic_parser(arithmetic_grammar, true);
+  typedef sh::lalr::parser_from_grammar<Grammar> Parser;
+  Parser parser(grammar, true);
   std::cout << "transition array:\n";
-  arithmetic_parser.dump_transition_array(std::cout);
+  parser.dump_transition_array(std::cout);
   // Tokens passed in to the parser one by one
-  arithmetic_parser.handle_token(X());
-  arithmetic_parser.handle_token(PLUS());
-  arithmetic_parser.handle_token(X());
+  parser.handle_token(X());
+  parser.handle_token(PLUS());
+  parser.handle_token(X());
   // And then we finalize to indicate the end of input, getting a parse object
   // encapsulating the result
-  parser_type::parse_type parse = arithmetic_parser.finalize();
+  Parser::parse_type parse = parser.finalize();
   // The parse object converted to bool yields success / failure
   if (!parse) {
     std::copy(
